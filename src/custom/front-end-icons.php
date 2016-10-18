4793d588a2e7250ac251ed3ba6358de20ae75bf7
Nate Finch <finchproservices@gmail.com>
2016-10-18 20:41:42 +0900

    front end displays only selected networks and in the correct order

--- src/custom/front-end-icons.php
+++ src/custom/front-end-icons.php
@@ -15,21 +15,50 @@
 $yass_options = get_option( 'yass_options' );
 
 //Bail out if plugin is deactivated
-if ($yass_options['yass_field_activate'] === 'activate') {
-	add_action('wp_footer', __NAMESPACE__ . '\add_yass_social_icons_floating_left');
+if ( $yass_options['yass_field_activate'] === 'activate' ) {
+	add_action( 'wp_footer', __NAMESPACE__ . '\add_yass_social_icons_floating_left' );
 }
 
-function build_the_yass_icons($yass_size_option, $yass_color_option) {
+function build_the_yass_icons( $yass_options, $yass_size_option, $yass_color_option, $dad_list ) {
+	$html = '<div class="yass-social-div">';
 
-	$html = '<div class="yass-social-div">
-		<span class="yass-icon-default ' . $yass_size_option . ' fa fa-facebook" ' .  $yass_color_option .'></span><br/>
-		<span class="yass-icon-default ' . $yass_size_option . ' fa fa-twitter" ' . $yass_color_option . '></span><br/>
-		<span class="yass-icon-default ' . $yass_size_option . ' fa fa-google-plus" '. $yass_color_option .'></span><br/>
-		<span class="yass-icon-default ' . $yass_size_option . ' fa fa-pinterest" '. $yass_color_option . '></span><br/>
-		<span class="yass-icon-default ' . $yass_size_option . ' fa fa-linkedin" ' . $yass_color_option . '></span><br/>';
-	if( wp_is_mobile()){
-		$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-whatsapp" '. $yass_color_option.'></span><br/>';
-	} //end wp_is_mobile
+	$yass_active_array = array();
+
+	foreach ( $dad_list as $key => $value ) {
+		if ( array_key_exists( 'yass_field_active_networks_' . $key, $yass_options ) ) {
+			array_push($yass_active_array, $key);
+		}
+	}
+
+
+	foreach ( $dad_list as $key => $value ) {
+
+		if ( in_array( $key, $yass_active_array ) ) {
+
+			switch ( $key ) {
+
+				case 'fb':
+					$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-facebook" ' . $yass_color_option . '></span><br/>';
+					break;
+				case 'tw':
+					$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-twitter" ' . $yass_color_option . '></span><br/>';
+					break;
+				case 'go':
+					$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-google-plus" ' . $yass_color_option . '></span><br/>';
+					break;
+				case 'pi':
+					$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-pinterest" ' . $yass_color_option . '></span><br/>';
+					break;
+				case 'li':
+					$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-linkedin" ' . $yass_color_option . '></span><br/>';
+					break;
+				case 'wa':
+					if ( wp_is_mobile() ) {
+						$html .= '<span class="yass-icon-default ' . $yass_size_option . ' fa fa-whatsapp" ' . $yass_color_option . '></span><br/>';
+					} //end wp_is_mobile
+			} // end switch
+		} // end if statement
+	} // end foreach
 	$html .= '</div>';
 
 	return $html;
@@ -38,23 +67,24 @@ function build_the_yass_icons($yass_size_option, $yass_color_option) {
 function add_yass_social_icons_floating_left() {
 
 	$yass_options = get_option( 'yass_options' );
+	$dad_list     = get_option( 'dad_list' );
 
 	//Color option
-	if ( $yass_options['yass_field_button_colors'] === 'default') {
+	if ( $yass_options['yass_field_button_colors'] === 'default' ) {
 		$yass_color_option = '';
 	} else {
-		$yass_color_option = 'style="color: '.$yass_options['yass_field_custom_icon_color'].'; background-color: '.$yass_options['yass_field_custom_background_color'].';"';
+		$yass_color_option = 'style="color: ' . $yass_options['yass_field_custom_icon_color'] . '; background-color: ' . $yass_options['yass_field_custom_background_color'] . ';"';
 	}
 
 	//Size
-	if ( $yass_options['yass_field_icon_size'] === 'medium') {
+	if ( $yass_options['yass_field_icon_size'] === 'medium' ) {
 		$yass_size_option = 'fa-2x';
-	} elseif ( $yass_options['yass_field_icon_size'] === 'large') {
+	} elseif ( $yass_options['yass_field_icon_size'] === 'large' ) {
 		$yass_size_option = 'fa-3x';
 	} else {
 		$yass_size_option = '';
 	}
 
-	echo build_the_yass_icons($yass_size_option, $yass_color_option);
+	echo build_the_yass_icons( $yass_options, $yass_size_option, $yass_color_option, $dad_list );
 
 }
